f332dbbdfdb3ec2aa90d9e13388b185b46984ab4
thierryiseli <thierry.iseli@tyup.ch>
2021-07-25 10:51:31 +0200

    [FIX] Fix upload.sh

--- upload.sh
+++ upload.sh
@@ -11,4 +11,4 @@ echo "uploading asset to release to url : $upload_url"
 curl -s -H "Authorization: token ${GITHUB_TOKEN}"  \
         -H "Content-Type: application/pdf" \
         --data-binary @${UPLOAD_FILE}  \
-        "$upload_url?name=${UPLOAD_FILE}&label=${UPLOAD_FILE}"   
+        "$upload_url\?name=${UPLOAD_FILE}\&label=${UPLOAD_FILE}"   
