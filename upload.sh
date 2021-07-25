d96be4d741e3b1103c77311c8b7ed590b0efa8e9
thierryiseli <thierry.iseli@tyup.ch>
2021-07-25 10:53:55 +0200

    [FIX] Fix upload.sh #2

--- upload.sh
+++ upload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 upload_url=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}"  \
-     -d "{\"tag_name\": \"${VERSION}\", \"name\":\"${RELEASE_NAME}}\",\"body\":\"${RELEASE_DESCRIPTION}\"}\"  \
+     -d "{\"tag_name\": \"${VERSION}\", \"name\":\"${RELEASE_NAME}}\",\"body\":\"${RELEASE_DESCRIPTION}\"}"  \
      "https://api.github.com/repos/${REPOSITORY_NAME}/releases" | jq -r '.upload_url')
 
-upload_url="${upload_url%\{*}"
+upload_url="${upload_url\%\{\*\}"
 
 echo "uploading asset to release to url : $upload_url"
 
